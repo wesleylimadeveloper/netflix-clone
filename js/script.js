bf7249bd148e235311d1fa755edab71c102f2b66
wesleahlima <wesleyguedes7@gmail.com>
2021-07-26 21:06:13 -0300

    Reacriando a interface da Netflix

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function assistirAgora() {
+    window.open('https://www.youtube.com/watch?v=ek1ePFp-nBI&ab_channel=UniversalPictures');
+}
+
+function maisInformacoes() {
+    window.open('https://www.themoviedb.org/movie/424139-halloween')
+}
